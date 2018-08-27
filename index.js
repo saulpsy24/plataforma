2f4c2bd1b086defd370bff5fe53167d06173823c
saulpsy24 <sarlmnt.soul@gmail.com>
2018-08-27 13:28:51 -0500

    Inicio del server

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict'
-var mongoose = require ('mongoose');
-mongoose.connect('mongodb://localhost:27017/plataforma',(err,res)=>
-{
-    if(err){
-        throw err;
-    }else{
-        console.log ("Conexion a la Base de datos corriendo correctamente");
-    }
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
+
+mongoose.connect('mongodb://localhost:27017/formacionescae',  (err,res) => {
+  if(err){
+      throw err;
+  } else{
+      console.log("La conexion a base esta corriendo correctamente");
+      app.listen(port,function(){
+          console.log("servidor del api rest online");
+      });
+  } 
+    
 });
